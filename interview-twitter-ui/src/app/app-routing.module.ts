ff02aec500bcdb93a6aa4602894c7aaf39f3acc7
harisjasar <hare.jasarevic@gmail.com>
2020-01-08 16:10:46 +0100

    updated import paths | error caused by moving the components to different directory

--- interview-twitter-ui/src/app/app-routing.module.ts
+++ interview-twitter-ui/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from "./auth/auth.guard";
-import { LogoutComponent } from './logout/logout.component';
+import { LogoutComponent } from './twitter/logout/logout.component';
 
 const routes: Routes = [
   {
--- interview-twitter-ui/src/app/app.module.ts
+++ interview-twitter-ui/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {HttpClientModule} from "@angular/common/http";
 import {AuthModule} from "./auth/auth.module";
-import { LogoutComponent } from './logout/logout.component';
-import { ProfileComponent } from './profile/profile.component';
+import { LogoutComponent } from './twitter/logout/logout.component';
+import { ProfileComponent } from './twitter/profile/profile.component';
 
 @NgModule({
   declarations: [
